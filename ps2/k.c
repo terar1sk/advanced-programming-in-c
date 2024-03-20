9c28dae8126cbf6a080b6cc0374a96e4112fa41e
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-03-20 14:00:58 +0100

    refresh

--- ps2/k.c
+++ ps2/k.c
@@ -4,7 +4,7 @@
 
 #include "k.h"
 
-/*
+
 void add_random_tile(struct game *game){
     int row, col;
     // find random, but empty tile
@@ -20,7 +20,7 @@ void add_random_tile(struct game *game){
         game->board[row][col] = 'B';
     }
 }
-*/
+
 
 bool is_game_won(const struct game current_game){
     for(int i = 0; i < SIZE; ++i){
@@ -31,5 +31,128 @@ bool is_game_won(const struct game current_game){
         }
     }
     return false;
+}
+
+
+bool is_move_possible(const struct game game){
+    for(int a = 0; a < SIZE; ++a){
+        for(int b = 0; b < SIZE; ++b){
+            if(game.board[a][b] == ' '){
+                return true;
+            }
+        }
+    }
+    for(int a = 0; a < SIZE; ++a){
+        for(int b = 0; b < SIZE - 1; ++b){
+            if(game.board[a][b] == game.board[a][b + 1]){
+                return true;
+            }
+        }
+    }
+    for(int a = 0; a < SIZE - 1; ++a){
+        for(int b = 0; b < SIZE; ++b){
+            if(game.board[a][b] == game.board[a + 1][b]){
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+
+
+bool update(struct game *game, int dy, int dx){
+    if(dy != -1 && dy != 0 && dy != 1) return false;
+    if(dx != -1 && dx != 0 && dx != 1) return false;
+    if(dy != 0 && dx != 0) return false;
+
+    bool moved = false;
+
+    if(dy == -1){
+        for(int j = 0; j < SIZE; ++j){
+            for(int i = 1; i < SIZE; ++i){
+                for(int k = i; k > 0; --k){
+                    if(game->board[k][j] == ' '){
+                        game->board[k][j] = game->board[k - 1][j];
+                        game->board[k - 1][j] = ' ';
+                        moved = true;
+                    }
+                    else if(game->board[k][j] == game->board[k - 1][j]){
+                        game->board[k][j]++;
+                        game->score += 1 << (game->board[k][j] - 'A');
+                        game->board[k - 1][j] = ' ';
+                        moved = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if(dy == 1){
+        for(int j = 0; j < SIZE; ++j){
+            for(int i = SIZE - 2; i >= 0; --i){
+                for(int k = i; k < SIZE - 1; ++k){
+                    if(game->board[k][j] == ' '){
+                        game->board[k][j] = game->board[k + 1][j];
+                        game->board[k + 1][j] = ' ';
+                        moved = true;
+                    }
+                    else if(game->board[k][j] == game->board[k + 1][j]){
+                        game->board[k][j]++;
+                        game->score += 1 << (game->board[k][j] - 'A');
+                        game->board[k + 1][j] = ' ';
+                        moved = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if(dx == -1){
+        for(int i = 0; i < SIZE; ++i){
+            for(int j = 1; j < SIZE; ++j){
+                for(int k = j; k > 0; --k){
+                    if(game->board[i][k] == ' '){
+                        game->board[i][k] = game->board[i][k - 1];
+                        game->board[i][k - 1] = ' ';
+                        moved = true;
+                    }
+                    else if(game->board[i][k] == game->board[i][k - 1]){
+                        game->board[i][k]++;
+                        game->score += 1 << (game->board[i][k] - 'A');
+                        game->board[i][k - 1] = ' ';
+                        moved = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if(dx == 1){
+        for(int i = 0; i < SIZE; ++i) {
+            for(int j = SIZE - 2; j >= 0; --j){
+                for(int k = j; k < SIZE - 1; ++k){
+                    if(game->board[i][k] == ' '){
+                        game->board[i][k] = game->board[i][k + 1];
+                        game->board[i][k + 1] = ' ';
+                        moved = true;
+                    }
+                    else if(game->board[i][k] == game->board[i][k + 1]){
+                        game->board[i][k]++;
+                        game->score += 1 << (game->board[i][k] - 'A');
+                        game->board[i][k + 1] = ' ';
+                        moved = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return moved;
 }
 
--- ps2/main.c
+++ ps2/main.c
@@ -5,19 +5,6 @@
 
 int main(){
 
-	// game is not won
-struct game game = {
-    .board = {
-        {'A', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', 'A'},
-        {'B', ' ', ' ', ' '}
-    },
-    .score = 0
-};
-
-printf("is won: %d\n", is_game_won(game));
-// stdout: 0
 
 return 0;
 }
