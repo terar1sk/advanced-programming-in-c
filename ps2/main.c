287ee45ef1f75cf9a2022e19707333b7025e2aad
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-03-20 17:14:36 +0100

    refresh

--- ps2/main.c
+++ ps2/main.c
@@ -1,10 +1,72 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "k.h"
+#include "ui.h"
 
+int main() {
+    struct game game = {
+        .board = {
+            {' ', ' ', ' ', 'A'},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', 'B'},
+            {' ', ' ', 'A', 'D'}
+        },
+        .score = 0
+    };
 
-int main(){
+    int y, x;
+    //add_random_tile(&game);
+    //add_random_tile(&game);
+    render(game);
+    do{
+        printf("\nEnter side: ");
+        scanf("%d %d", &y, &x);
+        if(update(&game, y, x)) {
+            add_random_tile(&game);
+            render(game);
+        }
+        else
+            printf("You can't move\n");
+    }while((y != 0 || x != 0) && is_move_possible(game) && is_game_won(game) == false);
+    if(is_game_won(game)) {
+        printf("You won!\n");
+    }else {
+        printf("You loose!\n");
+    }
+    
+    struct player list[10];
+    int count = load(list);
 
+        for(int i = 0; i < count; i++) {
+        printf("%s - %d\n", list[i].name, list[i].score);
+    }
 
-return 0;
+    struct player list2[10] = {
+        {"John", 150},
+        {"Genry", 170},
+        {"Aenry", 170}
+    };
+
+    save(list2, 3);
+
+    int size = 10;
+    struct player list3[10] = {
+        {"manager", 5000},
+        {"manager", 4000},
+        {"manager", 4000},
+        {"manager", 2000},
+        {"manager", 1000},
+        {"manager", 500},
+        {"manager", 400},
+        {"manager", 300},
+        {"manager", 200},
+        {"manager", 100},
+    };
+    struct player player = {.name="John", .score=100};;
+
+    add_player(list3, &size, player);
+    for(int i = 0; i < size; i++) {
+        printf("%s - %d\n", list3[i].name, list3[i].score);
+    }
+    return 0;
 }
--- ps2/ui.c
+++ ps2/ui.c
@@ -4,18 +4,20 @@
 
 #include "k.h"
 
-//void render(const struct game game){
-//    printf("score: %d\n", game.score);
-//
-//    for(int row = 0; row < SIZE; row++){
-//        printf("+---+---+---+---+\n|");
-//        for(int col = 0; col < SIZE; col++){
-//            printf(" %c |", game.board[row][col]);
-//        }
-//        printf("\n");
-//    }
-//    printf("+---+---+---+---+\n");
-//}
+void render(const struct game game){
+    printf("score: %d\n", game.score);
+
+    for(int row = 0; row < SIZE; row++){
+        printf("+---+---+---+---+\n|");
+        for(int col = 0; col < SIZE; col++){
+            printf(" %c |", game.board[row][col]);
+        }
+        printf("\n");
+    }
+    printf("+---+---+---+---+\n");
+}
+
+/*
 
 void render(const struct game game){
     clear();
@@ -56,3 +58,5 @@ void render(const struct game game){
 
     refresh();
 }
+
+*/
