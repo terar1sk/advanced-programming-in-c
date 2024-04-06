c3ffcc2604c1ab0b49d1e8420ff1c05b150a314f
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-04-06 16:52:49 +0200

    ps3

--- ps3/isolation.c
+++ ps3/isolation.c
@@ -1,57 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct tree{
-    int data;              
-    struct tree *left;  
-    struct tree *right; 
-} Tree;
-
-Tree *addtree(Tree *root, int nv){
-    if(root == NULL){
-        root = (Tree*)malloc(sizeof(Tree));
-        root->data = nv;
-        root->left = root->right = NULL;
-        return root;
-    }
-    if(root->data < nv){
-        root->right = addtree(root->right, nv);
-    }
-    else{
-    	root->left = addtree(root->left, nv);
-    }
-    return root;
-}
-
-void arraytree(Tree *root, int arr[]){
-    static int md = 0;
-    if(root == NULL){
-    return;
-    }             
-    arraytree(root->left, arr);     
-    arr[md++] = root->data;
-    arraytree(root->right, arr);      
-    free(root);
-}
-
-void sorttree(int arr[], int tt){
-    Tree *root;
-    int a;
-    root = NULL;
-    for(a = 0; a < tt; a++){
-    	root = addtree(root, arr[a]);
-    }
-    arraytree(root, arr);             
-}
-
 int main(){
-    int a;
-    int arr[19] = { 0, 7, 8, 3, 52, 14, 16, 18, 15, 13, 42, 30, 35, 26, 456, 5, 78, 99, 12345 };
+    int c1 = 20;
+    int c2 = 0;
+    int c3 = 0;
+    int u = 0;
+    int p = 0;
+    int desk[41][41];
+    for(int a = 0; a < 41; a++){
+        for(int b = 0; b < 41; b++){
+            desk[a][b]=228228;
+        }
+    }
+    scanf("%d", &u);
+    scanf("%d", &p);
+    int dn[u][p];
+    for(int a = 0; a < u; a++){
+        for(int b = 0; b < p; b++){
+            dn[a][b]=228228;
+        }
+    }
+    if(p == 1){
+        printf("%d",p);
+        return 0;
+    }
+    if(u == 1){
+        printf("1");
+        return 0;
+    }
+    int m1[u][p];
+    for(int n2 = 0; n2 < u; n2++){
+        for(int k2 = 0; k2 < p; k2++){
+            scanf("%d", &m1[n2][k2]);
+        }
+    }
+    for(int n1 = 0; n1 < u; n1++){
+        for(int k1 = 0; k1 < p; k1++){
+            c1 = 20;
+            c2 = 0;
+            int a = c1;
+            int q = c2;
+                    if(k1 == 0){
+                          desk[q][a]=m1[n1][k1];
+                        goto K1;
+                    }
 
-    sorttree(arr, 19);
-    for(a = 0; a < 19; a++) 
-        printf("%d ", arr[a]);
-    
+                if(desk[q][a]!=228228){
+                        for(int t=0; t<k1; t++){
+                            if(desk[q][a]==228228)
+                                break;
+                            if(m1[n1][k1] > desk[q][a]){
+                                a++;
+                                q++;
+                                continue;
+                            }
+                            else if(m1[n1][k1] < desk[q][a]){
+                                a--;
+                                q++;
+                                continue;
+                            }
+                        }
+                    }
+                    if(k1 == (p-1)){
+                        if(m1[n1][k1] > desk[q][a]){
+                              desk[q][a] = m1[n1][k1];
+                        }
+                        else if(m1[n1][k1] < desk[q][a]){
+                              desk[q][a] = m1[n1][k1];
+                        }
+                        for(int c = 0; c < 41; c++){
+                               for(int b = 0; b < 41; b++){
+                                   if(desk[c][b]!=228228){
+                                        dn[n1][c3] = b;
+                                         c3++;
+                                   }
+                               }
+                           }
+                          c3=0;
+                        for(int a = 0; a < 41; a++){
+                               for(int b = 0; b < 41; b++){
+                                     desk[b][a]=228228;
+                               }
+                           }
+                        goto K2;
+                    }
+
+
+                    if(desk[q][a]==228228){
+                          desk[q][a]=m1[n1][k1];
+                    }
+        K1:
+            continue;
+        }
+    K2:
+        continue;
+    }
+    int sum = u;
+    int sc = 0;
+    for(int s = 0; s < u; s++){
+        for(int q=(s+1); q < u; q++){
+            for(int e = 0; e < p; e++){
+                if(dn[s][e] == dn[q][e]){
+                    sc++;
+                }
+            }
+            if(sc == p){
+                sum--;
+            }
+            sc=0;
+        }
+    }
+    printf("%d\n", sum);
     return 0;
 }
 
--- ps3/racer.c
+++ ps3/racer.c
@@ -1,76 +1,63 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
-#include<math.h>
+#include <math.h>
 
 void calcul(int *size, int *c, int *d, int *f, float *border, float *frontier);
 float find(int *size, int *c, int *d, int *f, float *border, float *frontier);
 int test(int *size, int *c, int *d, int *f, float *g, float *e);
 
 void calcul(int *size, int *c, int *d, int *f, float *border, float *frontier){
-    float tp, e;
-    tp = f[0];
-    for(int a=0; a< *size; a++){
-        if(f[a]< tp){
-            tp = (float)f[a];
-        }
+    float sum_sv = 0, sum_v = 0;
+    for(int a = 0; a < *size; a++){
+        sum_sv += d[a] * f[a];
+        sum_v += f[a];
     }
-    
-    tp = -tp;
-    *border =tp;
-    while(true){
-        tp=tp+1;
-        if(!test(size, c, d, f, &tp, &e) || e >= 0.0){
-            *frontier = tp;
-            break;
-        }
-    }
+    float j = (sum_sv - *c * sum_v) / (sum_v - *size * *c);
+    *border = j;
+    *frontier = j;
 }
 
 int test(int *size, int *c, int *d, int *f, float *g, float *e){
     *e = 0.0;
-    for(int a=0; a<*size; a++){
-        *e += ((float)d[a]/((float)f[a]+*g));
+    for(int a = 0; a < *size; a++){
+        *e += ((float)d[a] / ((float)f[a] + *g));
     }
     *e -= (float)*c;
-    return fabs(*e) <= 1e-9;
+    return fabs(*e) <= 1e-6;
 }
 
 float find(int *size, int *c, int *d, int *f, float *border, float *frontier){
-    float nr = 0.0;
-    float e = 0.0;
-    while(true){
-        nr = (*border + *frontier) / 2;
-        if(test(size, c, d, f, &nr, &e)){
-            return nr;
+    float left = *border, right = *frontier, middle;
+    float e;
+    while(right - left > 1e-6){
+        middle = left + (right - left) / 2;
+        if(test(size, c, d, f, &middle, &e)){
+            right = middle;
         }
         else{
-            if(e > 0){
-                *border = nr;
-            }
-            else{
-                *frontier = nr;
-            }
-        }
+            left = middle;
+   	}
     }
+    return left + (right - left) / 2;
 }
 
 int main(){
-    int c=0;
-    int size=0;
-    int d[199999]={0};
-    int f[199999]={0};
-    long double g=0.0;
-    float border=0.0;
-    float frontier=0.0;
+    int c = 0;
+    int size = 0;
+    int d[1000] = {0};
+    int f[1000] = {0};
+    long double g = 0.0;
+    float border = 0.0;
+    float frontier = 0.0;
 
     scanf("%d %d", &size, &c);
-    for(int i=0; i<size; i++){
-        scanf("%d %d", &d[i], &f[i]);
+    for(int a = 0; a < size; a++){
+        scanf("%d %d", &d[a], &f[a]);
     }
     calcul(&size, &c, d, f, &border, &frontier);
     g = find(&size, &c, d, f, &border, &frontier);
-    printf("%Lf\n",g);
-    
+    printf("%.9Lf\n", g);
+
     return 0;
 }
 
