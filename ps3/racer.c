19035c25a0c287b044af884e5503c9644dedccc3
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-04-07 17:34:33 +0200

    ps3

--- ps3/racer.c
+++ ps3/racer.c
@@ -7,57 +7,83 @@ float find(int *size, int *c, int *d, int *f, float *border, float *frontier);
 int test(int *size, int *c, int *d, int *f, float *g, float *e);
 
 void calcul(int *size, int *c, int *d, int *f, float *border, float *frontier){
-    float sum_sv = 0, sum_v = 0;
-    for(int a = 0; a < *size; a++){
-        sum_sv += d[a] * f[a];
-        sum_v += f[a];
+  float tm, e;
+  tm = f[0];
+  
+  for(int a = 0; a < *size; a++){
+    if(f[a] < tm){
+      tm = (float)f[a];
     }
-    float j = (sum_sv - *c * sum_v) / (sum_v - *size * *c);
-    *border = j;
-    *frontier = j;
+  }
+  tm = -tm;
+  *border = tm;
+
+  while(true){
+    tm = tm + 1;
+    test(size, c, d, f, &tm, &e);
+
+    if(e < 0.0){
+      *frontier = tm;
+      break;
+    }
+  }
 }
 
 int test(int *size, int *c, int *d, int *f, float *g, float *e){
-    *e = 0.0;
-    for(int a = 0; a < *size; a++){
-        *e += ((float)d[a] / ((float)f[a] + *g));
-    }
-    *e -= (float)*c;
-    return fabs(*e) <= 1e-6;
+  *e = 0.0;
+
+  for(int a = 0; a < *size; a++){
+    *e += ((float)d[a] / ((float)f[a] + *g));
+  }
+  *e -= (float)*c;
+  if(*e <= 0.0000000009 && *e >= -0.0000000001){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
 
 float find(int *size, int *c, int *d, int *f, float *border, float *frontier){
-    float left = *border, right = *frontier, middle;
-    float e;
-    while(right - left > 1e-6){
-        middle = left + (right - left) / 2;
-        if(test(size, c, d, f, &middle, &e)){
-            right = middle;
-        }
-        else{
-            left = middle;
-   	}
+  float nn =0.0;
+  float e =0.0;
+
+  while(true){
+    nn = (*border + *frontier) / 2;
+    if(test(size, c, d, f, &nn, &e)){
+      return nn;
     }
-    return left + (right - left) / 2;
+    else{
+      if(e > 0){
+        *border=nn;
+      }
+      if(e < 0){
+        *frontier = nn;
+      }
+    }
+  }
+  return 0;
 }
 
 int main(){
-    int c = 0;
-    int size = 0;
-    int d[1000] = {0};
-    int f[1000] = {0};
-    long double g = 0.0;
-    float border = 0.0;
-    float frontier = 0.0;
+  int c = 0;
+  int size = 0;
+  int d[199999] = {0};
+  int f[199999] = {0};
+  long double g = 0.0;
+  float border = 0.0;
+  float frontier = 0.0;
 
-    scanf("%d %d", &size, &c);
-    for(int a = 0; a < size; a++){
-        scanf("%d %d", &d[a], &f[a]);
-    }
-    calcul(&size, &c, d, f, &border, &frontier);
-    g = find(&size, &c, d, f, &border, &frontier);
-    printf("%.9Lf\n", g);
+  scanf("%d %d", &size, &c);
+  for(int a = 0; a < size; a++){
+    scanf("%d %d", &d[a], &f[a]);
+  }
 
-    return 0;
+  calcul(&size, &c, d, f, &border, & frontier);
+  g = find(&size, &c, d, f, &border, &frontier);
+  printf("%Lf\n",g);
+
+  return 0;
 }
+
 
