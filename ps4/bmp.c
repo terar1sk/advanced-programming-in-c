5f9ed8c47fbdaf186e5915dc95e8131b2591d948
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-04-19 15:47:50 +0200

    ps4

--- ps4/bmp.c
+++ ps4/bmp.c
@@ -29,70 +29,46 @@ struct bmp_header* read_bmp_header(FILE* stream){
   if(stream == NULL){
     return NULL;
   }
-  struct bmp_header *header;
-  unsigned int headersize;
-  int width;
-  int height;
-  unsigned short planes;
-  unsigned int compression;
-  unsigned int size_image;
-  int x_ppm;
-  int y_ppm;
-  unsigned int c_u;
-  unsigned int c_i;
-  headersize = (unsigned int) R4(stream);
-  width = (int) R4(stream);
-  height = (int) R4(stream);
-  planes = (unsigned short) R2(stream);
-  compression = (unsigned int) R4(stream);
-  size_image = (unsigned int) R4(stream);
-  x_ppm = (int) R4(stream);
-  y_ppm = (int) R4(stream);
-  c_u = (unsigned int) R4(stream);
-  c_i = (unsigned int) R4(stream);
-
-  char file_type[3] = {'\0', '\0', '\0'};
-  unsigned int fs;
-  unsigned short booked;
-  unsigned short armored;
-  unsigned long bs;
-  fread(&file_type, 1, 2, stream);
-  fs = (unsigned int) R4(stream);
-  fread(&booked, 2, 1, stream);
-  fread(&armored, 2, 1, stream);
-  bs = (unsigned long) R4(stream);
-  header = (struct bmp_header*) malloc(sizeof(struct bmp_header));
-  header->size = fs;
-  header->reserved1 = booked;
-  header->reserved2 = armored;
-  header->offset = bs;
-  header->size = headersize;
-  header->width = width;
-  header->height = height;
-  header->planes = planes;
-  header->compression = compression;
-  header->image_size = size_image;
-  header->x_ppm = x_ppm;
-  header->y_ppm = y_ppm;
-  header->num_colors = c_u;
-  header->important_colors = c_i;
+  struct bmp_header *header = malloc(sizeof(struct bmp_header));
+  if(header == NULL){
+    return NULL;
+  }
+  header->size = (unsigned int) R4(stream);
+  header->width = (int) R4(stream);
+  header->height = (int) R4(stream);
+  header->planes = (unsigned short) R2(stream);
+  header->compression = (unsigned int) R4(stream);
+  header->image_size = (unsigned int) R4(stream);
+  header->x_ppm = (int) R4(stream);
+  header->y_ppm = (int) R4(stream);
+  header->num_colors = (unsigned int) R4(stream);
+  header->important_colors = (unsigned int) R4(stream);
   return header;
 }
 
 struct bmp_image* read_bmp(FILE* stream){
   struct bmp_header *header = read_bmp_header(stream);
-  struct pixel *pixel = read_data(stream, header);
-  struct bmp_image *image;
-  image = malloc(sizeof(struct bmp_image*));
   if(header == NULL){
     fprintf(stdout,"Error: Corrupted BMP file.\n");
     return NULL;
   }
   else if(header->type != 0x424D){
     fprintf(stdout,"Error: This is not a BMP file.\n");
+    free(header);
     return NULL;
   }
-  image->data = malloc(sizeof(*image->data) * image->header->image_size);
+  struct bmp_image *image = malloc(sizeof(struct bmp_image));
+  if(image == NULL){
+    free(header);
+    return NULL;
+  }
+  image->header = header;
+  image->data = malloc(sizeof(struct pixel) * header->image_size);
+  if(image->data == NULL){
+    free(header);
+    free(image);
+    return NULL;
+  }
   return image;
 }
 
@@ -100,26 +76,26 @@ bool write_bmp(FILE* stream, const struct bmp_image* image){
   if(stream == NULL || image == NULL){
     return false;
   }
-  fwrite(image, sizeof(image->header->size),1,stream);
-  fwrite(image->data, sizeof(image->data), 1,stream);
+  fwrite(image->header, sizeof(struct bmp_header), 1, stream);
+  fwrite(image->data, sizeof(struct pixel), image->header->image_size, stream);
   return true;
 }
 
 struct pixel* read_data(FILE* stream, const struct bmp_header* header){
-  if(stream == NULL){
+  if(stream == NULL || header == NULL){
     return NULL;
   }
-  if(header == NULL){
+  struct pixel *pixel = malloc(sizeof(struct pixel) * header->height * header->width);
+  if(pixel == NULL){
     return NULL;
   }
-  struct pixel *pixel = malloc(header->height * header->width);
   return pixel;
 }
 
 void free_bmp_image(struct bmp_image* image){
   if(image){
     free(image->data);
+    free(image->header);
     free(image);
   }
 }
-
--- ps4/transformations.c
+++ ps4/transformations.c
@@ -5,36 +5,240 @@
 #include <string.h>
 
 struct bmp_image* flip_horizontally(const struct bmp_image* image){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+  if(new_image == NULL){
+    return NULL;
+  }
+  new_image->header = malloc(sizeof(struct bmp_header));
+  if(new_image->header == NULL){
+    free(new_image);
+    return NULL;
+  }
+  memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+  new_image->data = malloc(sizeof(struct pixel) * image->header->image_size);
+  if(new_image->data == NULL){
+    free(new_image->header);
+    free(new_image);
+    return NULL;
+  }
+  int width = image->header->width;
+  int height = image->header->height;
+  for(int a = 0; a < height; a++){
+    for(int b = 0; b < width; b++){
+      new_image->data[a * width + b] = image->data[a * width + (width - b - 1)];
+    }
+  }
+  return new_image;
 }
 
 struct bmp_image* flip_vertically(const struct bmp_image* image){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+    if(new_image == NULL){
+      return NULL;
+    }
+    new_image->header = malloc(sizeof(struct bmp_header));
+    if(new_image->header == NULL){
+      free(new_image);
+      return NULL;
+    }
+    memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+    new_image->data = malloc(sizeof(struct pixel) * image->header->image_size);
+    if(new_image->data == NULL){
+      free(new_image->header);
+      free(new_image);
+      return NULL;
+    }
+    int width = image->header->width;
+    int height = image->header->height;
+    for(int a = 0; a < height; a++){
+      for(int b = 0; b < width; b++){
+        new_image->data[a * width + b] = image->data[(height - a - 1) * width + b];
+      }
+    }
+  return new_image;
 }
 
 struct bmp_image* rotate_right(const struct bmp_image* image){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+    if(new_image == NULL){
+      return NULL;
+    }
+    new_image->header = malloc(sizeof(struct bmp_header));
+    if(new_image->header == NULL){
+      free(new_image);
+      return NULL;
+    }
+    memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+    new_image->data = malloc(sizeof(struct pixel) * image->header->image_size);
+    if(new_image->data == NULL){
+      free(new_image->header); 
+      free(new_image);
+      return NULL;
+    }
+    int width = image->header->width;
+    int height = image->header->height;
+    for(int a = 0; a < height; a++){
+      for(int b = 0; b < width; b++){
+        new_image->data[b * height + (height - a - 1)] = image->data[a * width + b];
+      }
+    }
+  new_image->header->width = height;
+  new_image->header->height = width;
+  return new_image;
 }
 
 struct bmp_image* rotate_left(const struct bmp_image* image){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+  if(new_image == NULL){
+    return NULL;
+  }
+  new_image->header = malloc(sizeof(struct bmp_header));
+  if(new_image->header == NULL){
+    free(new_image);
+    return NULL;
+  }
+  memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+  new_image->data = malloc(sizeof(struct pixel) * image->header->image_size);
+  if(new_image->data == NULL){
+    free(new_image->header); 
+    free(new_image);
+    return NULL;
+  }
+  int width = image->header->width;
+  int height = image->header->height;
+  for(int a = 0; a < height; a++){
+    for(int b = 0; b < width; b++){
+      new_image->data[(width - b - 1) * height + a] = image->data[a * width + b];
+    }
+  }
+  new_image->header->width = height;
+  new_image->header->height = width;
+  return new_image;
 }
 
 struct bmp_image* scale(const struct bmp_image* image, float factor){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL || factor <= 0){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+  if(new_image == NULL){
+    return NULL;
+  }
+  new_image->header = malloc(sizeof(struct bmp_header));
+  if(new_image->header == NULL){
+    free(new_image);
+    return NULL;
+  }
+  memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+  new_image->header->width = (int)(image->header->width * factor);
+  new_image->header->height = (int)(image->header->height * factor);
+  new_image->data = malloc(sizeof(struct pixel) * new_image->header->image_size);
+  if(new_image->data == NULL){
+    free(new_image->header);
+    free(new_image);
+    return NULL;
+  }
+  int width = image->header->width;
+  int height = image->header->height;
+  int new_width = new_image->header->width;
+  int new_height = new_image->header->height;
+  for(int a = 0; a < new_height; a++){
+    for(int b = 0; b < new_width; b++){
+      int original_i = (int)(a / factor);
+      int original_j = (int)(b / factor);
+      new_image->data[a * new_width + b] = image->data[original_i * width + original_j];
+    }
+  }
+  return new_image;
 }
 
 struct bmp_image* crop(const struct bmp_image* image, const int start_y, const int start_x, const int height, const int width){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL ||
+  start_y < 0 || start_x < 0 || height <= 0 || width <= 0 ||
+  start_y + height > image->header->height || start_x + width > image->header->width){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+    if(new_image == NULL){
+      return NULL;
+    }
+    new_image->header = malloc(sizeof(struct bmp_header));
+    if(new_image->header == NULL){
+      free(new_image); 
+      return NULL;
+    }
+    memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+    new_image->header->width = width;
+    new_image->header->height = height;
+    new_image->data = malloc(sizeof(struct pixel) * height * width);
+    if(new_image->data == NULL){
+      free(new_image->header);
+      free(new_image);
+      return NULL;
+    }
+    int original_width = image->header->width;
+    int original_height = image->header->height;
+    for(int a = 0; a < height; a++){
+      for(int b = 0; b < width; b++){
+        new_image->data[a * width + b] = image->data[(start_y + a) * original_width + (start_x + b)];
+      }
+    }
+  return new_image;
 }
 
 struct bmp_image* extract(const struct bmp_image* image, const char* colors_to_keep){
-  image = malloc(sizeof(*image));
-  return NULL;
+  if(image == NULL || image->header == NULL || image->data == NULL || colors_to_keep == NULL){
+    return NULL;
+  }
+  struct bmp_image* new_image = malloc(sizeof(struct bmp_image));
+  if(new_image == NULL){
+    return NULL;
+  }
+  new_image->header = malloc(sizeof(struct bmp_header));
+  if(new_image->header == NULL){
+    free(new_image);
+    return NULL;
+  }
+  memcpy(new_image->header, image->header, sizeof(struct bmp_header));
+
+  new_image->data = malloc(sizeof(struct pixel) * image->header->image_size);
+  if(new_image->data == NULL){
+    free(new_image->header); 
+    free(new_image);
+    return NULL;
+  }
+  int width = image->header->width;
+  int height = image->header->height;
+  int new_size = 0;
+  for(int a = 0; a < height; a++){
+    for(int b = 0; b < width; b++){
+      struct pixel current_pixel = image->data[a * width + b];
+      char* color_ptr = strchr(colors_to_keep, current_pixel.color);
+      if(color_ptr != NULL){
+        new_image->data[new_size++] = current_pixel;
+      }
+    }
+  }
+  new_image->header->image_size = new_size;
+  new_image->header->width = new_size / height;
+  new_image->header->height = height;
+  return new_image;
 }
