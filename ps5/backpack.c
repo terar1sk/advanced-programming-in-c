5f43544beeddc939f6d16fa1f981198433833525
Dmytro Isai <dmytro.isai@student.tuke.sk>
2024-05-03 20:14:12 +0200

    ps5

--- ps5/backpack.c
+++ ps5/backpack.c
@@ -8,25 +8,40 @@
 
 
 struct backpack* create_backpack(const int capacity){
-  return NULL;
+  struct backpack * new_back_pack = calloc(1, sizeof(struct backpack));
+  new_back_pack->capacity = capacity;
+  return new_back_pack;
 }
-
 
 struct backpack* destroy_backpack(struct backpack* backpack){
+  if(backpack != NULL){
+    free(backpack);
+  }
   return NULL;
 }
-
 
 bool add_item_to_backpack(struct backpack* backpack, struct item* item){
-  return NULL;
+	if(backpack==NULL){
+		return false;
+	}
+	if(item==NULL || backpack->capacity==backpack->size){
+		return false;
+	}
+	backpack->items=create_container(backpack->items,ITEM,item);
+	backpack->size++;
+	return true;
 }
-
 
 void delete_item_from_backpack(struct backpack* backpack, struct item* item){
-
+	backpack->items=remove_container(backpack->items,item);
+	backpack->size--;
 }
 
-
 struct item* get_item_from_backpack(const struct backpack* backpack, char* name){
-  return NULL;
+  if(backpack != NULL && backpack->items != NULL && name != NULL && strlen(name) > 0){
+    return get_from_container_by_name(backpack->items, name);
+  }
+  else{
+    return NULL;
+  }
 }
